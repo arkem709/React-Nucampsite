eb79d2d3989448fde1c1a7772c50cc2f1753e235
Aleef Mahmud <aleef.mahmud@gmail.com>
2022-12-10 10:36:37 -0500

    CampsitesDirectoryPage and CampsiteDetail~

--- src/features/campsites/campsitesSlice.js
+++ src/features/campsites/campsitesSlice.js
@@ -1,3 +1,9 @@
+import { CAMPSITES } from "../../app/shared/CAMPSITES";
+
+export const selectAllCampsites = () => {
+  return CAMPSITES;
+};
+
 export const selectRandomCampsite = () => {
-    return CAMPSITES[Math.floor(CAMPSITES.length * Math.random())];
+  return CAMPSITES[Math.floor(CAMPSITES.length * Math.random())];
 };
