af835c50e649bcd4b2e95a6be39412b44d45665b
Aleef Mahmud <aleef.mahmud@gmail.com>
2022-12-24 13:26:09 +0100

    Week 5 Workshop Assignment

--- src/features/comments/CommentsList.js
+++ src/features/comments/CommentsList.js
@@ -1,13 +1,36 @@
-import { Col } from 'reactstrap';
+import { Col, Row } from 'reactstrap';
 import Comment from './Comment';
 import { selectCommentsByCampsiteId } from './commentsSlice';
 import CommentForm from './CommentForm';
 import { useSelector } from 'react-redux';
+import Error from '../../components/Error';
+import Loading from '../../components/Loading';
 
 const CommentsList = ({ campsiteId }) => {
+    
     const comments = useSelector(selectCommentsByCampsiteId(campsiteId));
 
+    const isLoading = useSelector((state) => state.comments.isLoading);
+    const errMsg = useSelector((state) => state.comments.errMsg);
+
     if (comments && comments.length > 0) {
+        
+        if (isLoading) {
+            return (
+              <Row>
+                <Loading />
+              </Row>
+            ); 
+          }
+      
+          if (errMsg) {
+            return (
+              <Row>
+                <Error errMsg={errMsg} />
+              </Row>
+            )
+          }  
+
         return (
             <Col md='5' className='m-1'>
                 <h4>Comments</h4>
